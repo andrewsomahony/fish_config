19a093d2bbd2268183137067ae7bf139ef85ea83
Andrew O'Mahony <andrewsomahony@gmail.com>
2025-06-23 03:27:03 +0000

    Initial Commit

---
+++ config.fish
@@ -0,0 +1,43 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+    set -U fish_color_normal B3B1AD
+    set -U fish_color_command 39BAE6
+    set -U fish_color_keyword 39BAE6
+    set -U fish_color_quote C2D94C
+    set -U fish_color_redirection FFEE99
+    set -U fish_color_end F29668
+    set -U fish_color_error FF3333
+    set -U fish_color_param B3B1AD
+    set -U fish_color_comment 626A73
+    set -U fish_color_match F07178
+    set -U fish_color_selection --background=E6B450
+    set -U fish_color_search_match --background=E6B450
+    set -U fish_color_history_current --bold
+    set -U fish_color_operator E6B450
+    set -U fish_color_escape 95E6CB
+    set -U fish_color_cwd 59C2FF
+    set -U fish_color_cwd_root red
+    set -U fish_color_option B3B1AD
+    set -U fish_color_valid_path --underline
+    set -U fish_color_autosuggestion 4D5566
+    set -U fish_color_user brgreen
+    set -U fish_color_host normal
+    set -U fish_color_host_remote yellow
+    set -U fish_color_history_current --bold
+    set -U fish_color_status red
+    set -U fish_color_cancel --reverse
+    set -U fish_pager_color_prefix normal --bold --underline
+    set -U fish_pager_color_progress brwhite --background=cyan
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description B3A06D
+    set -U fish_pager_color_selected_background --background=E6B450
+    set -U fish_pager_color_secondary_background 
+    set -U fish_pager_color_secondary_completion 
+    set -U fish_pager_color_background 
+    set -U fish_pager_color_selected_completion 
+    set -U fish_pager_color_selected_description 
+    set -U fish_pager_color_selected_prefix 
+    set -U fish_pager_color_secondary_description 
+    set -U fish_pager_color_secondary_prefix
+end
