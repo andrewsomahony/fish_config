c2b81c5eed1bce23b09c920927d07f562ae93038
Andrew O'Mahony <andrew.omahony@saronic.com>
2025-06-29 17:41:24 -0500

    Don't check for bound container if we are inside a container

--- functions/_prompt_bound_container.fish
+++ functions/_prompt_bound_container.fish
@@ -2,10 +2,16 @@ function _prompt_bound_container
     # Run our BASH script to detect if the current directory is
     # bound to a container.
   
-    bash ~/dev/container/detect_with_error.sh
+    # If we are in a container, we don't need to check this
+    # !!! I suppose we should one day, but container-within-container
+    # !!! isn't really something that we use at this time
 
-    if test $status -eq 0
-        echo "$pure_symbol_bound_container_prefix" #(_pure_user_at_host)
+    if _pure_is_inside_container
+      bash ~/dev/container/detect_with_error.sh
+
+      if test $status -eq 0
+          echo "$pure_symbol_bound_container_prefix" #(_pure_user_at_host)
+      end
     end
 end
 
