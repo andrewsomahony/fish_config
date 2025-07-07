080a307470a244733a28d5b5c26f548396841ba2
Andrew O'Mahony <andrewsomahony@gmail.com>
2025-07-07 15:11:41 +0000

    Local shell can only exist on top of a devshell

--- functions/localshell.fish
+++ functions/localshell.fish
@@ -2,8 +2,13 @@ function localshell
   # Run nix develop in the local directory, which will create the
   # Nix devshell using the "default" shell
   if not test -e ./flake.nix
-    echo "No flake.nix file in current directory"
+    echo "No flake.nix file in current directory."
   else
-    nix develop --impure .
+    set --local shell_level "$SHLVL"
+    if test $shell_level -gt 1
+      nix develop --impure .
+    else
+      echo "Local shell can only exist on top of a devshell."
+    end
   end
 end
