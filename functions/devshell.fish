46586e3d30e708aa73123490fe3434972b5a85b5
Andrew O'Mahony <andrewsomahony@gmail.com>
2025-07-20 19:18:08 +0000

    Localshell can specify which shell, if there are multiple options

--- functions/devshell.fish
+++ functions/devshell.fish
@@ -1,7 +1,4 @@
 function devshell
-  # !!! TODO: Check the current directory for a flake.nix file, otherwise use
-  # !!! the home directory file
-
   # Run our Nix develop command, using the flake in our home directory
   nix develop --impure ~/dev --command fish
 end
--- functions/localshell.fish
+++ functions/localshell.fish
@@ -1,6 +1,15 @@
-function localshell
-  # Run nix develop in the local directory, which will create the
-  # Nix devshell using the "default" shell
+# !!! TODO: Need to parse arguments and open the corresponding shell
+# !!! instead of "default"
+function localshell -a shell_name
+  if test -z "$shell_name"
+    # Set our default shell name
+    set --local shell_name "default"
+  end
+
+  # Set our Nix path to the shell we have picked
+  set --local shell_path ".#$shell_name"
+
+  # Run nix develop in the local directory
   if not test -e ./flake.nix
     echo "No flake.nix file in current directory."
   else
@@ -20,7 +29,7 @@ function localshell
         mkdir $temporary_directory
         TMPDIR=$temporary_directory nix develop --impure --command fish
       else
-        nix develop --impure --command fish
+        nix develop --impure $shell_path --command fish
       end
     else
       echo "Local shell can only exist on top of a devshell."
